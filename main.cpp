0573b547872e5632b51a78a9effcb26f920ba090
empty-works <pama.mark@gmail.com>
2020-05-12 06:45:48 +0200

    Initialized ifstream and ofstream objects and checked if files can be opened.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 #include <fstream>
 
 int main() {
-	
+	std::ifstream in_file{"romeojuliet.txt"};
+	std::ofstream out_file{"romeojuliet_out.txt"};
+	if(!in_file) {
+		std::cerr << "Error loading in file." << std::endl;
+		return 1;
+	}
+	if(!out_file) {
+		std::cerr << "Error loading out file." << std::endl;
+		return 1;
+	}
+			
 	return 0;
 }
