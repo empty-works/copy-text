f874cd7979bfe8040dd77fb65d5897ae03e78118
empty-works <pama.mark@gmail.com>
2020-05-12 07:16:10 +0200

    Copied text to out file displays all lines with numbers. Only applied to lines with text.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main() {
 	std::ifstream in_file{"romeojuliet.txt"};
@@ -12,6 +13,14 @@ int main() {
 		std::cerr << "Error loading out file." << std::endl;
 		return 1;
 	}
-			
+	std::string line;
+	int line_num{1};
+	while(std::getline(in_file, line)) {
+		if(line.find_first_not_of(' ') != std::string::npos) { // A non-space has been found.
+			out_file << line_num << " " << line << std::endl;
+			line_num++;
+		}
+	}		
+	std::cout << "Copy complete." << std::endl;
 	return 0;
 }
